145f51148899b3d072ab7318472ee396b0d3b4d3
tima gutmensch <tima_gutmensch+github@proton.me>
2025-10-03 08:26:01 +0800

    chore(setup): Centralize dependency injection loading

--- lib/core/utils/injections.dart
+++ lib/core/utils/injections.dart
@@ -1,2 +1,10 @@
 import 'package:get_it/get_it.dart';
+import 'package:homecare_mobile/core/network/network_injections.dart';
+import 'package:homecare_mobile/core/storage/storage_injections.dart';
+import 'package:homecare_mobile/features/auth/authentication_injections.dart';
 final sl = GetIt.instance;
+Future<void> initInjections() async {
+  await initStorageInjections();
+  await initAppInjections();
+  await initNetworkInjections();
+}
