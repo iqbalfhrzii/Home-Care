16e7745abaa01a749ec5f4a283a44a2594a89290
tima gutmensch <tima_gutmensch+github@proton.me>
2025-10-10 09:15:49 +0800

    refactor: switch splash page to go_router with simple loader

--- lib/shared/presentation/pages/splash_page.dart
+++ lib/shared/presentation/pages/splash_page.dart
@@ -1,58 +1,30 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:homecare_mobile/core/router/app_router.dart';
 import 'package:homecare_mobile/features/auth/presentation/bloc/auth_bloc.dart';
-import 'package:homecare_mobile/main.gr.dart';
 
-@RoutePage()
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
-
 
   @override
   Widget build(BuildContext context) {
     return BlocListener<AuthBloc, AuthState>(
       listener: _authBlocListener,
-      child: const Scaffold(
-        backgroundColor: Color(0xFFE8EAF6),
-        body: _SplashView(),
-      ),
+      child: const Scaffold(body: Center(child: CircularProgressIndicator())),
     );
   }
 
   void _authBlocListener(BuildContext context, AuthState state) {
     if (state is AuthAuthenticated) {
-      context.router.replace(const MainRoute());
+      context.go(AppRouter.home);
     } else if (state is AuthUnauthenticated) {
-      context.router.replace(const LoginRoute());
+      context.go(AppRouter.login);
+    } else if (state is AuthError) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(state.message)));
+      context.go(AppRouter.login);
     }
-    // If AuthError, decide what to do (maybe show error, then go to login)
-    if (state is AuthError) {
-      context.router.replace(const LoginRoute());
-    }
-  }
-}
-
-class _SplashView extends StatelessWidget {
-  const _SplashView();
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'assets/images/idHW0aFfe7_logos.png',
-            width: 250,
-            height: 100,
-          ),
-          const SizedBox(height: 20),
-          const CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF3F51B5)),
-          ),
-        ],
-      ),
-    );
   }
 }
