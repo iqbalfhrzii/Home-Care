7373b544f17d226869d0155bc7b4a89a696a3e74
tima gutmensch <tima_gutmensch+github@proton.me>
2025-10-10 09:16:37 +0800

    refactor: rename ReportPage to ReportListPage and strip auto_route

--- lib/features/reports/report_list_page.dart
+++ lib/features/reports/report_list_page.dart
@@ -1,14 +1,10 @@
 // lib/pages/report_page.dart
-import 'package:auto_route/auto_route.dart';
+// import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
-@RoutePage()
-class ReportPage extends StatelessWidget {
-  const ReportPage({super.key});
-  // static const List<Widget> _widgetOptions = <Widget>[
-  //   SchedulePage(),
-  //   ReportPage(),
-  // ];
+// @RoutePage()
+class ReportListPage extends StatelessWidget {
+  const ReportListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,14 +16,7 @@ class ReportPage extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.logout),
             tooltip: 'Logout', // Teks yang muncul saat ikon ditekan lama
-            onPressed: () {
-              // Navigasi kembali ke halaman login
-              // 'popAndPushNamed' akan menghapus semua halaman sebelumnya dari stack
-              Navigator.of(context).pushNamedAndRemoveUntil(
-                '/', // Rute untuk login_page.dart
-                (Route<dynamic> route) => false,
-              );
-            },
+            onPressed: () {},
           ),
         ],
       ),
