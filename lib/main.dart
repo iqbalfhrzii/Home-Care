7dbc3d3db5dd3e707a51b4bff5dfae9aaba89d40
tima gutmensch <tima_gutmensch+github@proton.me>
2025-10-20 16:35:09 +0800

    feat: apply Rspb colorscheme and font

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:homecare_mobile/core/router/app_router.dart';
+import 'package:homecare_mobile/core/theme/theme.dart';
 import 'package:homecare_mobile/core/utils/injections.dart';
 import 'package:homecare_mobile/core/utils/logger.dart';
 import 'package:homecare_mobile/features/auth/presentation/bloc/auth_bloc.dart';
@@ -33,13 +34,24 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    // final brightness = View.of(context).platformDispatcher.platformBrightness;
+
+    // Retrieves the default theme for the platform
+    //TextTheme textTheme = Theme.of(context).textTheme;
+
+    // Use with Google Fonts package to use downloadable fonts
+    TextTheme textTheme = createTextTheme(context, "Roboto", "Montserrat");
+
+    MaterialTheme theme = MaterialTheme(textTheme);
+
     logger.d('Building MyApp widget');
 
     return BlocProvider<AuthBloc>.value(
       value: sl<AuthBloc>()..add(const AuthCheckRequested()),
       child: MaterialApp.router(
         title: 'Aplikasi Percobaan',
-        theme: ThemeData(primarySwatch: Colors.blue),
+        // theme: brightness == Brightness.light ? theme.light() : theme.dark(),
+        theme: theme.light(),
         routerConfig: _appRouter.router,
       ),
     );
